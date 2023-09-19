615229c027797e89ee0746b3b51b246d5d5365a8
lucas <ls.gomes1994@gmail.com>
2023-09-19 01:12:35 -0300

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import token from "./helpers/token"
 import datas from "./helpers/datas"
 import formatarPrecos from "./helpers/formatarPrecos"
 import mobile from './helpers/mobile'
 import validacoes from "./helpers/validacoes"
 import formatarTextos from "./helpers/formatarTextos"
 
-exports.helperToken = token
 exports.helperPreco = formatarPrecos
 exports.helperData = datas
 exports.helperMobile = mobile
